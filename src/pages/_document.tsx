0b7b3d09b3ab247171fab2008af6be25eee08c1d
Edmund <edmundlzen@gmail.com>
2023-09-22 11:26:57 +0800

    Added cool pokemon icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,8 +14,12 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="logo.ico" type="image/x-icon" />
-        <link rel="shortcut icon" href="logo.ico" type="image/x-icon" />
+        <link
+          type="image/png"
+          sizes="96x96"
+          rel="icon"
+          href="icons8-pokeball-96.png"
+        />
       </Head>
       <body>
         <Main />
